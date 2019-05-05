98943fb4740e2885b5f8fe18dae20d1ba875cee5
JRobsonJr <jrobsonjr16@gmail.com>
2019-05-05 17:37:29 -0300

    Updates

---
+++ precache-manifest.0896b79d06bd3d0ff7ed8480b3fd7315.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0832ae387302fb57d269e84bfa85622a",
+    "url": "/ts-ranked/index.html"
+  },
+  {
+    "revision": "fb33d5cf7cb8f04fba8f",
+    "url": "/ts-ranked/static/css/main.4054ce2e.chunk.css"
+  },
+  {
+    "revision": "933bcb9035d13a35300b",
+    "url": "/ts-ranked/static/js/2.315b8043.chunk.js"
+  },
+  {
+    "revision": "fb33d5cf7cb8f04fba8f",
+    "url": "/ts-ranked/static/js/main.a1811860.chunk.js"
+  },
+  {
+    "revision": "8a5b3bdea06a1372b415",
+    "url": "/ts-ranked/static/js/runtime~main.b75a0fe1.js"
+  },
+  {
+    "revision": "91cbb1d3e6012ab9505d19e4083c6208",
+    "url": "/ts-ranked/static/media/noah-bold-webfont.91cbb1d3.woff2"
+  },
+  {
+    "revision": "ad43452d892e737323088f7818b6f745",
+    "url": "/ts-ranked/static/media/noah-bold-webfont.ad43452d.woff"
+  },
+  {
+    "revision": "2d048c4d95446635b57dd296dbcb4187",
+    "url": "/ts-ranked/static/media/noah-regular-webfont.2d048c4d.woff"
+  },
+  {
+    "revision": "3e5382722ade47a4f084dd702e80fc51",
+    "url": "/ts-ranked/static/media/noah-regular-webfont.3e538272.woff2"
+  },
+  {
+    "revision": "82f63d8826327b1b5d60ce04695928cf",
+    "url": "/ts-ranked/static/media/noah-regularitalic-webfont.82f63d88.woff2"
+  },
+  {
+    "revision": "c6f22776ab138177e245c1b795f9c375",
+    "url": "/ts-ranked/static/media/noah-regularitalic-webfont.c6f22776.woff"
+  },
+  {
+    "revision": "b7b6640d164f0cbc749a83bfce10292f",
+    "url": "/ts-ranked/static/media/taylor.b7b6640d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/taylorswiftranked/precache-manifest.0d3838cb1d594d3ed0210aab29c9f699.js"
+  "/ts-ranked/precache-manifest.0896b79d06bd3d0ff7ed8480b3fd7315.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taylorswiftranked/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ts-ranked/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
