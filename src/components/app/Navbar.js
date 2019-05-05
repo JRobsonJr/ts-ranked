ce14dcf3a233690f121d197490e2eb764151e2f4
JRobsonJr <jrobsonjr16@gmail.com>
2019-05-05 12:57:00 -0300

    Update Navbar styles and add link to ranking

--- src/components/app/Navbar.js
+++ src/components/app/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './Navbar.css';
+
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-light">
         <a className="navbar-brand" href="/">
@@ -12,29 +14,33 @@ const Navbar = () => (
 
 const NavbarCollapseButton = () => (
     <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span className="navbar-toggler-icon" />
-        </button>
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+    >
+        <span className="navbar-toggler-icon" />
+    </button>
 );
 
 const NavbarLinkList = () => (
-
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-            <a className="nav-link" href="/">
-                HOME <span className="sr-only">(current)</span>
-            </a>
-        </li>
-    </ul>
-</div>
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+                <a className="nav-link" href="/">
+                    HOME <span className="sr-only">(current)</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/ranking">
+                    RANKING
+                </a>
+            </li>
+        </ul>
+    </div>
 );
 
 export default Navbar;
