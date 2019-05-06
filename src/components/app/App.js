ad8d5db55ab7be414a67eeb699b3092f6f1a3739
JRobsonJr <jrobsonjr16@gmail.com>
2019-05-05 22:59:39 -0300

    Create HomePage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Navbar from './Navbar';
+import HomePage from '../home/HomePage';
 import SelectionPage from '../selection-page/SelectionPage';
 import OrderingPage from '../ordering-page/OrderingPage';
 import RankingPage from '../ranking-page/RankingPage';
@@ -10,7 +11,8 @@ const App = () => (
     <>
         <Navbar />
         <Switch>
-            <Route exact path="/" component={SelectionPage} />
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/select" component={SelectionPage} />
             <Route exact path="/order" component={OrderingPage} />
             <Route exact path="/ranking" component={RankingPage} />
         </Switch>
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './HomePage.css';
+
+const HomePage = () => (
+    <div className="mx-1">
+        <MainSection />
+        <AboutSection />
+        <RestrictionsSection />
+        <ContactSection />
+        <StartSection />
+    </div>
+);
+
+const MainSection = () => (
+    <div className="container container-home text-center">
+        <h1 className="display-2">Taylor Swift Ranker</h1>
+        <h3>
+            Singer-songwriter Taylor Swift has released six studio albums and a
+            handful of singles over the course of her career.
+        </h3>
+        <h5>
+            Can you choose your favorite <b>13</b> songs out of her discography?
+        </h5>
+    </div>
+);
+
+const AboutSection = () => (
+    <div className="container container-home">
+        <div className="row">
+            <div className="col-lg-6">
+                <h1 className="display-4">And that's how it works...</h1>
+                <h4>
+                    It's pretty simple: select 13 songs and, in the next step,
+                    reorder them from favorite to least favorite. In the end,
+                    you'll be able to generate a cool image you can use to share
+                    on social media. Here's an example of how it should look
+                    like: my own ranking!
+                </h4>
+                <h5>
+                    If you share this on Twitter, use the hashtag{' '}
+                    <b>#TaylorSwiftRanked</b> so that I can check your ranking!
+                </h5>
+                <h5>
+                    If you'd like to contribute with the overall ranking, after
+                    generating your list, you'll be given the option to{' '}
+                    <i>"Submit your ranking"</i>. Let's see how your favorite
+                    tracks compare to other people's favorites!
+                </h5>
+            </div>
+            <div className="col-lg-6 align-self-center">
+                <img
+                    src="assets/tsranked-top13-example.png"
+                    className="ranking-example"
+                    alt="My ranking"
+                />
+            </div>
+        </div>
+    </div>
+);
+
+const RestrictionsSection = () => (
+    <section className="container container-home">
+        <h1>
+            Wait, where's <i>[unreleased 2005 demo song]</i>?
+        </h1>
+        <h5>
+            Note that only tracks that were officially released by Taylor are
+            listed. Another little restriction was to only include songs in
+            which she was credited as a main artist. The last one is that all of
+            her officially released covers are not listed (except for{' '}
+            <i>Untouchable</i>, since she earned a writing credit for it).
+        </h5>
+    </section>
+);
+
+const ContactSection = () => (
+    <section className="container container-home">
+        <h1>Contact</h1>
+        <div className="row">
+            <div className="col-lg-7">
+                <h5>
+                    As a fellow Swiftie, find me at <b>Twitter</b>. Hey, let's
+                    be friends!
+                </h5>
+                <h5>
+                    As a developer, you can find me on <b>GitHub</b>. If you
+                    have any issues using this tool, please, contact me!
+                </h5>
+            </div>
+            <div className="col-lg-5">
+                <SocialMediaButton
+                    text="Twitter (@SoftCircuits)"
+                    href="https://twitter.com/SoftCircuits"
+                />
+                <SocialMediaButton
+                    text="GitHub (/JRobsonJr)"
+                    href="https://github.com/JRobsonJr"
+                />
+            </div>
+        </div>
+    </section>
+);
+
+const SocialMediaButton = ({ text, href }) => (
+    <a
+        className="btn btn-block btn-outline-dark badge-pill"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href}
+    >
+        {text}
+    </a>
+);
+
+const StartSection = () => (
+    <section className="container container-home text-center">
+        <h5>I don't think you should wait. I think you should...</h5>
+        <Link className="btn btn-lg btn-outline-dark badge-pill" to="/select">
+            START NOW!
+        </Link>
+    </section>
+);
+
+export default HomePage;
