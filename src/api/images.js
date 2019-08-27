f38ce4497d2801267e9eac851abddff99580f622
JRobsonJr <jrobsonjr16@gmail.com>
2019-08-26 22:47:49 -0300

    Add endpoint to post image to imgur

---
+++ src/api/images.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://api.imgur.com',
+    headers: { Authorization: 'Client-ID 9ba26223a321650' },
+});
+
+export const postRankingToImgur = async node => {
+    const blob = await getRankingBlob(node);
+    const response = await axiosInstance.post('/3/upload/', blob);
+    return response.data.data.link;
+};
+
+export const saveRankingAsPng = async node => {
+    const blob = await getRankingBlob(node);
+    saveAs(blob, 'tsranked-top13.png');
+};
+
+const getRankingBlob = async node => {
+    const blob = await domtoimage.toBlob(node, {
+        style: { height: '100%', top: '0', position: 'relative', left: '0' },
+        bgcolor: '#f5676c',
+    });
+    return blob;
+};
