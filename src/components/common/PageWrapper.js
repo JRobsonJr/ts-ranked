592053b32016654d8dc951fa1db00fe7264701f1
JRobsonJr <jrobsonjr16@gmail.com>
2019-08-29 21:45:32 -0300

    Create page wrapper styles

--- src/components/common/PageWrapper.js
+++ src/components/common/PageWrapper.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './PageWrapper.css';
+
 const PageWrapper = ({ children }) => (
     <div className="mx-1">
-        <div className="container ranking-page shadow p-4 rounded-lg">
+        <div className="container page-wrapper shadow p-4 rounded-lg">
             {children}
         </div>
     </div>
